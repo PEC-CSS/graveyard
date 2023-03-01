9753dbb194401d2101a3f1477fc4ca0f6041c925
Akanksha Narula <akanksha.narula8@gmail.com>
2023-03-02 01:18:33 +0530

    Issue56

---
+++ ml/issue56.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+data = {'birds': ['Cranes', 'Cranes', 'plovers', 'spoonbills', 'spoonbills', 'Cranes', 'plovers', 'Cranes', 'spoonbills', 'spoonbills'], 'age': [3.5, 4, 1.5, np.nan, 6, 3, 5.5, np.nan, 8, 4], 'visits': [2, 4, 3, 4, 3, 4, 2, 2, 3, 2], 'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+birds_df = pd.DataFrame(data, index=labels)
+print(birds_df)
