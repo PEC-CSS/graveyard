6b0314b2c9c7d8b4010a7bffa495fc2ae843e728
Prakul06 <prakulbansal.bt22cse@pec.edu.in>
2023-03-03 00:22:32 +0530

    N and its Double

--- misc/N and its Double.c
+++ misc/N and its Double.c
@@ -1,46 +1,29 @@
 int main()
 {
     int N,a,i,j,count=0;
-    printf("Enter N=");
-    scanf("%d",&N);
-    if(N>=2 & N<=500)
+    int A[500];
+    for(a=0;a<N;a++)
     {
-        int A[500];
-        for(a=0;a<N;a++)
+        printf("Enter Value at A[%d]=",a);
+        scanf("%d",&A[a]);
+    }
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
         {
-            printf("Enter Value at A[%d]=",a);
-            scanf("%d",&A[a]);
-            if(A[a]<=103 & A[a]>=-103)
-            i++;
-            else
+            if(i!=j)
             {
-                printf("Invalid Input");
-                exit(0);
-            }
-        }
-        for(i=0;i<N;i++)
-        {
-            for(j=0;j<N;j++)
-            {
-                if(i!=j)
+                if(A[i]==2*A[j])
                 {
-                    if(A[i]==2*A[j])
-                    {
-                        printf("True");
-                        count++;
-                        break;
-                    }
+                    printf("True\n");
+                    count++;
+                    break;
                 }
             }
-            if(count==1)
-            break;
         }
-        if(count==0)
-        printf("False");
-
+        if(count==1)
+        break;
     }
-    else
-    {
-        printf("Invalid Value of N");
-    }
+    if(count==0)
+    printf("False\n");
 }
