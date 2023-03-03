9c607f96862093502dc1802c9600fc36bb0f30aa
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-03-03 11:31:19 +0530

    Operator overloading go brrr... #110

--- misc/complex.py
+++ misc/complex.py
@@ -7,3 +7,17 @@ class complex:
     return (self.real**2+self.imag**2)**0.5
   def arg(self):
     return atan(self.imag/self.real)
+  def __add__(self, other):
+    r = self.real + other.real
+    i = self.imag + other.imag
+    return complex(r,i)
+  def __sub__(self, other):
+    r = self.real - other.real
+    i = self.imag - other.imag
+    return complex(r,i)
+  def __mul__(self, other):
+    a, b = self.real, self.imag
+    c, d = other.real, other.imag
+    r = a*c - b*d
+    i = a*d + b*c
+    return complex(r,i)
