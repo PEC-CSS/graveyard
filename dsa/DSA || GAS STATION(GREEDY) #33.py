5f06f97a7b097236d39a8bdf385eb72961edb554
GauravGupta993 <123398315+GauravGupta993@users.noreply.github.com>
2023-03-03 14:34:43 +0000

    DSA  || GAS STATION(GREEDY) #33

--- dsa/DSA || GAS STATION(GREEDY) #33.py
+++ dsa/DSA || GAS STATION(GREEDY) #33.py
@@ -1,4 +1,4 @@
-def gascheck(j, counter, remaining_gas, cost_for_next_station,n):
+def gascheck(j, counter, remaining_gas, cost_for_next_station,n,gas,cost):
     if (counter == n):
         return 106
     elif (remaining_gas >= cost_for_next_station):
@@ -8,14 +8,14 @@ def gascheck(j, counter, remaining_gas, cost_for_next_station,n):
         remaining_gas = gas[j] + remaining_gas - cost_for_next_station
         cost_for_next_station = cost[j]
         counter = counter + 1
-        if((gascheck(j, counter, remaining_gas, cost_for_next_station,n))==106):
+        if((gascheck(j, counter, remaining_gas, cost_for_next_station,n,gas,cost))==106):
             return 106
 gas = list(map(int, input().split()))
 cost = list(map(int, input().split()))
 n = len(gas)
 index = -1
 for i in range(n):
-    if (gascheck(i, 0, gas[i], cost[i],n) == 106):
+    if (gascheck(i, 0, gas[i], cost[i],n,gas,cost) == 106):
         index = i
         break
 print(index)
