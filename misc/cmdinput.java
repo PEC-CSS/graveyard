2f48e254da419d9290334717c1599ea45ee3326b
Dharyaj11 <dharyajasuja2003@gmail.com>
2023-03-04 23:47:06 +0530

    Taking input from command line in Java

--- misc/cmdinput.java
+++ misc/cmdinput.java
@@ -1,8 +1,14 @@
 public class cmdinput {
-    public static void main(String args[]){  
+    public static void main(String args[]){ 
+        int maxi=0; 
   
-        for(int i=0;i<args.length;i++)  
-        System.out.println(args[i]);  
-          
+        for(int i=0;i<args.length;i++) 
+        { 
+            if(maxi<Integer.parseInt(args[i]))
+            maxi=Integer.parseInt(args[i]);
+
+
+        } 
+        System.out.println("maximum value is "+maxi);  
         }  
 }
