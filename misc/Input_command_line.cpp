5aaf41daff221eeb07003be05f0ad21a69a33e04
himanshusab12 <119474384+himanshusab12@users.noreply.github.com>
2023-03-04 15:56:43 +0530

    Update Input_command_line.cpp

--- misc/Input_command_line.cpp
+++ misc/Input_command_line.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char **argv){
-    if (argc<2){
-        cout<<0;
-    }
-    else{
-        int max = 0;
-        for(int i=1; i<argc; i++){
-            if(atoi(argv[i])>max){
-                max = atoi(argv[i]);
-            }
+    int max = 0;
+    for(int i=1; i<argc; i++){
+        if(atoi(argv[i])>max){
+            max = atoi(argv[i]);
         }
-        cout<<max;
-    } 
+    }
+    cout<<max;
     return 0;
 }
