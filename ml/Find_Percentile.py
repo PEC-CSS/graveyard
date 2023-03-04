710288c28093eb6d677ca1b5ee7e4b1c8aa8c36e
himanshusab12 <119474384+himanshusab12@users.noreply.github.com>
2023-03-04 22:01:19 +0530

    Update Find_Percentile.py

--- ml/Find_Percentile.py
+++ ml/Find_Percentile.py
@@ -8,7 +8,6 @@ for a in range(m*n):
     l.append(x)
 arr = numpy.array(l)
 arr = arr.reshape(m,n)
-print(arr)
 print(p,"th percentile of flattened array is ", numpy.percentile(arr,p),sep="")
 print(p,"th percentile values of rows are ",numpy.percentile(arr,p,axis=0),sep="")
 print(p,"th percentile values of columns are ",numpy.percentile(arr,p,axis=1),sep="")
