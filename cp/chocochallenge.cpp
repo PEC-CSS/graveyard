d4c1ebc186e9e01ecf0ab01ff7130bf3f835658f
Abhishek Sood <nyp.abhishek@gmail.com>
2023-03-05 16:25:15 +0530

    chocochallenge #15

---
+++ cp/chocochallenge.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    int xor = 0;
+    int max_chocs = 0;
+    int count_max_chocs = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        xor ^= a[i];
+
+        if (a[i] > max_chocs)
+        {
+            max_chocs = a[i];
+            count_max_chocs = 1;
+        }
+        else if (a[i] == max_chocs)
+        {
+            count_max_chocs++;
+        }
+    }
+    if (xor != 0 || count_max_chocs > 1)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == max_chocs)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
