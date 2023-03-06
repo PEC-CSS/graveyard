45682f37f1b4842ab87f645ad2cf331aba632710
himanshusab12 <119474384+himanshusab12@users.noreply.github.com>
2023-03-06 17:45:47 +0530

    Update Find_Percentile.py

--- ml/Find_Percentile.py
+++ ml/Find_Percentile.py
@@ -2,12 +2,11 @@ import numpy
 m = int(input("Enter m"))
 n = int(input("Enter n"))
 p = int(input("Enter p"))
-l = []
-for a in range(m*n):
-    x = int(input(f"Enter num {a+1}"))
-    l.append(x)
-arr = numpy.array(l)
-arr = arr.reshape(m,n)
+arr = numpy.zeros((m,n),dtype=int)
+for a in range(m):
+    for b in range(n):
+        arr[a][b]= int(input(f"Enter num"))
+print(arr)
 print(p,"th percentile of flattened array is ", numpy.percentile(arr,p),sep="")
 print(p,"th percentile values of rows are ",numpy.percentile(arr,p,axis=0),sep="")
 print(p,"th percentile values of columns are ",numpy.percentile(arr,p,axis=1),sep="")
