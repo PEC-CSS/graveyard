560bf54cd7f3fd658416fd7220ceda5f2dd14849
GauravGupta993 <123398315+GauravGupta993@users.noreply.github.com>
2023-03-06 16:52:30 +0000

    Write a command line application for tic-tac-toe #132

--- dsa/tic-tac-toe.py
+++ dsa/tic-tac-toe.py
@@ -3,19 +3,43 @@ def tictactoe_board(list1,list2,list3,i):
     if(i%2==0):
         n=int(input("x turn. Enter location in 1-9 where you want to enter:"))
         if (n == 1 or n == 2 or n == 3):
-            list1[n-1]="x"
+            if(list1[n-1]=="x"or list1[n-1]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list1[n-1]="x"
         if (n == 4 or n == 5 or n == 6):
-            list2[n-4]="x"
+            if (list2[n-4]=="x"or list2[n-4]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list2[n - 4] = "x"
         if (n == 7 or n == 8 or n == 9):
-            list3[n-7]="x"
+            if (list3[n-7]=="x"or list3[n-7]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list3[n - 7] = "x"
     elif(i%2!=0):
         n=int(input("o turn. Enter location in 1-9 where you want to enter:"))
         if (n == 1 or n == 2 or n == 3):
-            list1[n-1]="o"
+            if (list1[n-1]=="x"or list1[n-1]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list1[n - 1] = "o"
         if (n == 4 or n == 5 or n == 6):
-            list2[n-4]="o"
+            if (list2[n-4]=="x"or list2[n-4]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list2[n - 4] = "o"
         if (n == 7 or n == 8 or n == 9):
-            list3[n-7]="o"
+            if (list3[n-7]=="x"or list3[n-7]=="o"):
+                print("Place already occupied")
+                i=i-1
+            else:
+                list3[n - 7] = "o"
     print(list1[0], list1[1], list1[2], sep="")
     print(list2[0], list2[1], list2[2], sep="")
     print(list3[0], list3[1], list3[2], sep="")
