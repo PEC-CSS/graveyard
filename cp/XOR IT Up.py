42f74201e46c0630c327318a0c713ead0ecd99d7
GauravGupta993 <123398315+GauravGupta993@users.noreply.github.com>
2023-03-06 16:07:27 +0000

    XOR IT UP #185

---
+++ cp/XOR IT Up.py
@@ -0,0 +1,19 @@
+def func(lis):
+    answer=lis[0]^lis[1]
+    for i in range(2,len(lis)):
+        answer=answer^lis[i]
+    return answer
+
+no_of_testcases=int(input())
+dictionary=dict()
+for i in range(no_of_testcases):
+    length=int(input())
+    arri=list(map(int,input().split()))
+    dictionary.update({i:arri})
+for i in range(no_of_testcases):
+    combined_array=dictionary[i]
+    for j in range(len(combined_array)):
+        x = combined_array.pop(j)
+        if(func(combined_array)==x):
+            print(x)
+            break
