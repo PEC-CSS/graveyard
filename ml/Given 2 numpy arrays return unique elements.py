7d768a1fd2bf5c7e3a6618ee90b329ad03ef3518
aadityavvij <119474270+aadityavvij@users.noreply.github.com>
2023-03-06 12:53:52 +0530

    updated issue #52

--- ml/Given 2 numpy arrays return unique elements.py
+++ ml/Given 2 numpy arrays return unique elements.py
@@ -4,6 +4,6 @@ array1 = np.array([1,2,3,4,5,6,7])
 array2 = np.array([3,2,3,8,6,6,7])
 
 if(array1.size==array2.size):
-    print(np.unique(array1[np.where(array1!=array2)]))
+    print(array1[array1 != array2])
 else:
     print("Not Comparable")
