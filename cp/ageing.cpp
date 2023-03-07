5bf5b69031f94d22028c1d67c46bbb35040abd48
Krittika <krittikajari.bt22cse@pec.edu.in>
2023-03-07 17:25:42 +0530

    Changes made in issue #72

--- cp/ageing.cpp
+++ cp/ageing.cpp
@@ -5,14 +5,11 @@ int main()
     int T;
     cin>>T;
     int x[T];
-    if(T>=1 && T<=25)
-    {
     for(int i = 0;i<T;i++)
     {
             {
                 cin>>x[i];
             }
-    }
     }
     for(int j = 0; j<T;j++)
         {
