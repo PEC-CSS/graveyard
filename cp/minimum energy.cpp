481c3129e42dfa2189fc08772d58572eae51c48e
Krittika <krittikajari.bt22cse@pec.edu.in>
2023-03-13 00:48:21 +0530

    Done #184

--- cp/minimum energy.cpp
+++ cp/minimum energy.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 int main()
 {
-    int rows,columns,min_energy;
-    cin>>rows>>columns;
-    if (rows<columns){
-        min_energy = (2*rows)+columns-2;
+    int T;
+    cin>>T;
+    int row[T];
+    int column[T];
+    int min_energy;
+    for(int i  = 0; i<T;i++)
+    {
+    cin>>row[i]>>column[i];
+    }
+    for(int i = 0;i<T;i++)
+    {
+    if (row[i]<column[i]){
+        min_energy = (2*row[i])+column[i]-2;
     }
     else
     {
-        min_energy = rows+(columns*2)-2;
+        min_energy = row[i]+(column[i]*2)-2;
     }
-    cout<<min_energy;
-
+    cout<<min_energy<<endl;
+    }
     return 0;
 }
