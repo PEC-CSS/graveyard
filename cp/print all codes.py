8d0cea5b50fdd0ac1775505cf360e74d726afb3b
ragss69 <spideyrags@gmail.com>
2023-03-15 21:44:31 +0530

    issue #206

--- cp/print all codes.py
+++ cp/print all codes.py
@@ -1,15 +1,17 @@
 def print_all_codes(s,s2):
-    if(len(s)==1):
+    if(len(s)==1 and 1<=int(s[0])):
         print(s2+chr(int(s[0])+96))
     elif(len(s)==2):
-        print(s2+chr(int(s[0])+96)+chr(int(s[1])+96))
-        if(int(s[0])*10+int(s[1])<=26):
+        if(1<=int(s[0]) and 1<=int(s[1])):
+            print(s2+chr(int(s[0])+96)+chr(int(s[1])+96))
+        if(1<=int(s[0]) and (int(s[0])*10+int(s[1])<=26)):
             print(s2+chr(int(s[0])*10+int(s[1])+96))
     else:
-        print_all_codes(s[1:],s2+chr(int(s[0])+96))
-        if(int(s[0])*10+int(s[1])<=26):
+        if(1<=int(s[0])):
+            print_all_codes(s[1:],s2+chr(int(s[0])+96))
+        if(1<=int(s[0]) and (int(s[0])*10+int(s[1])<=26)):
             print_all_codes(s[2:],s2+chr(int(s[0])*10+int(s[1])+96))
         
-s=[]
+
 s=input()
 print_all_codes(s,"")
